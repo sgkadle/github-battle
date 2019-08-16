eb87f66055a6be04369d75827a25bdf63bdbc35a
Sai Kadle <sai.kadle@gmail.com>
2019-08-16 14:05:52 -0700

    learning props and states

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import './index.css'
+import Poplular from './components/Popular'
 
 class App extends React.Component{
 	render() {
 		return (
-			<div>
-				I am building Github Battle App in React!
+			<div className='container'>
+				<Poplular />
 			</div>
 		)
 	}
